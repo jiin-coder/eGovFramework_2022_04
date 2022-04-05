391fbdc61613ddb43e12509b64ed997471076ff5
jiin-coder <fpahstiq123@naver.com>
2022-04-05 21:49:03 +0900

    Controller와 jsp의 데이터 전달, 조회

--- src/main/java/egov/main/web/MainController.java
+++ src/main/java/egov/main/web/MainController.java
@@ -3,15 +3,85 @@ package egov.main.web;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
 	@RequestMapping(value="/main.do")
-	public String main(HttpServletRequest request, ModelMap model){
-		return "main/main";
+	public String main(HttpServletRequest request, Model model){
+		return "main";
 	}
+	
+	
+	@RequestMapping(value="/main2.do")
+	public String main2(HttpServletRequest request, Model model){
+		return "main/main2";
+	}
+		
+	
+	// form으로부터 입력받은 데이트를 가공 및 처리 1
+	
+	 /*
+	  * id를 user1 입력시 : userNo+5 한 값과 입력한 id가 나오도록
+	  * 이 외 : userNo+5 한 값과 "잘못된 입력"이란 글자가 나오도록
+	  */
+//	@RequestMapping(value="/main3.do")
+//	public String main3(HttpServletRequest request, ModelMap model){
+//		
+//		int userNo = Integer.parseInt(request.getParameter("userNo").toString());
+//		String id = request.getParameter("id").toString();
+//		
+//		if(id.equals("user1")){
+//			model.addAttribute("userId", id);
+//		}
+//		else{
+//			model.addAttribute("userId", "잘못된 입력");
+//		}
+//		
+//		userNo = userNo + 5;
+//		model.addAttribute("userNo", userNo);
+//		
+//		return "main/main3";
+//	}
+
+	
+	// form으로부터 입력받은 데이트를 가공 및 처리 1
+	// id와 pw를 input으로 제출 시, userNo와 pw 나오도록
+	@RequestMapping(value="/main3.do")
+	public String main3(@RequestParam("pw")String pw, HttpServletRequest request, ModelMap model){
+		
+		int userNo = Integer.parseInt(request.getParameter("userNo").toString());
+		String id = request.getParameter("id").toString();
+		
+		if(id.equals("user1")){
+			model.addAttribute("userId", pw);
+		}
+		else{
+			model.addAttribute("userId", pw);
+		}
+		
+		userNo = userNo + 5;
+		model.addAttribute("userNo", userNo);
+		
+		return "main/main3";
+	}
+	
+	
+	
+	// @PathVariable를 이용해 userNo를 path(Url 경로)의 일부로 파라미터 사용
+	// http://localhost:8080/Egov_WEB/main4/23123123.do
+	@RequestMapping(value="/main4/{userNo}.do")
+	public String main4(@PathVariable String userNo, HttpServletRequest request, ModelMap model){
+		
+		model.addAttribute("userNo", userNo);
+		
+		return "main/main3";
+	}
+
 }
 
