f07f75e41200cdd20e85961b2e9f66f31d7c389c
jiin-coder <fpahstiq123@naver.com>
2022-04-12 13:15:16 +0900

    @AspectJ 어노테이션 기반 AOP 활용

--- src/main/java/egov/lib/util/ExampleAop.java
+++ src/main/java/egov/lib/util/ExampleAop.java
@@ -2,18 +2,32 @@ package egov.lib.util;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Aspect
 public class ExampleAop {
 
 	private static final Logger logger = LoggerFactory.getLogger(ExampleAop.class);
 
+	@Pointcut("execution(* egov.**.web.*Controller.*(..))")
+	public void aspectMethod(){
+		
+	}
+	
+	@Before("aspectMethod()")
 	public void beforeMethod(JoinPoint joinPoint) throws Exception {
 		logger.info("사용자 요청" + joinPoint.getTarget());
 		System.out.println("사용자 요청" + joinPoint.getTarget());
 	}
 
+	@AfterThrowing(pointcut = "aspectMethod()", throwing = "exception")
 	public void afterExceptionMethod(JoinPoint joinPoint, Exception exception) throws Exception {
 		// 이외에도 예외처리에는 많은 방법이 존재합니다. ErrorController로 제어등..
 		logger.error("ST에러발생=================");
@@ -23,10 +37,12 @@ public class ExampleAop {
 		throw exception;
 	}
 
+	@After("aspectMethod()")
 	public void afterMethod(JoinPoint joinPoint) throws Exception {
 		logger.info("메소드 종료");
 	}
 
+	@Around("aspectMethod()")
 	public Object aroundMethod(ProceedingJoinPoint joinPoint) throws Throwable {
 
 		// 시간 측정
